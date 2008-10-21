1df503e718df60558f49b4f8b309331d8c9ee797
Sven Herzberg <sven@imendio.com>
2008-10-21 17:36:34 +0200

    added a window to the client

--- demo/client.c
+++ demo/client.c
@@ -23,10 +23,23 @@
  * if advised of the possibility of such damage.
  */
 
+#include <gtk/gtk.h>
+
 int
 main (int   argc,
       char**argv)
 {
+        GtkWidget* window;
+
+        gtk_init (&argc, &argv);
+
+        window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+        g_signal_connect (window, "destroy",
+                          G_CALLBACK (gtk_main_quit), NULL);
+
+        gtk_widget_show_all (window);
+        gtk_main ();
+
         return 0;
 }
 
