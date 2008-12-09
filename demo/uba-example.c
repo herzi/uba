5b67f5b4c4fb5730be1f0c199f37f7b44f935cda
Sven Herzberg <sven@imendio.com>
2008-12-09 15:02:51 +0100

    added a main loop for the example service binary

--- demo/uba-example.c
+++ demo/uba-example.c
@@ -21,10 +21,18 @@
  * USA
  */
 
+#include <glib.h>
+
 int
 main (int   argc,
       char**argv)
 {
+        GMainLoop* loop;
+
+        loop = g_main_loop_new (NULL, FALSE);
+
+        g_main_loop_run (loop);
+        g_main_loop_unref (loop);
         return 0;
 }
 
